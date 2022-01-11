2e89063650686aa3acee8411eec316c589dc27b7
Fabian Ardila <fabianra97@gmail.com>
2022-01-11 12:59:40 -0500

    :construction: Buils items with data

--- meli_scraper/meli_scraper/items.py
+++ meli_scraper/meli_scraper/items.py
@@ -8,5 +8,8 @@ import scrapy
 
 class MeliScraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    name = scrapy.Field()
+    price = scrapy.Field()
+    reviews = scrapy.Field()
+    free_fees = scrapy.Field()
+    
--- meli_scraper/meli_scraper/spiders/phone_spider.py
+++ meli_scraper/meli_scraper/spiders/phone_spider.py
@@ -1,5 +1,9 @@
 import scrapy
 
+from ..items import MeliScraperItem
+
+# title: //li[contains(@class, "item")]/div[contains(@class, "wrapper")]/div/div[contains(@class, "content-wrapper")]/div[contains(@class, "title")]/a/h2/text()
+
 class PhoneSpider(scrapy.Spider):
     name = 'meli'
     start_urls = [
@@ -10,16 +14,33 @@ class PhoneSpider(scrapy.Spider):
         'FEED_EXPORT_ENCODING': 'utf-8'
     }
 
+    def parse_items(self, response, **kwargs):
+        items = kwargs['items']
+
+        print('*'*10)
+        print('\n\n')
+        # TODO: pensar si se puede traer sólo los FULL
+        items_list = response.xpath('//div[@class="ui-search-result__content-wrapper"]')
+        for idx, item_selection in enumerate(items_list):
+            is_full = item_selection.xpath('.//span[@class="ui-search-item__fulfillment"]').get()
+            if is_full:
+                item = MeliScraperItem()
+                item['name'] = item_selection.xpath('./div[contains(@class, "title")]/a/h2/text()').get()
+                item['price'] = item_selection.xpath('.//span[@class="price-tag-fraction"]/text()').get()
+                item['reviews'] = item_selection.xpath('.//span[@class="ui-search-reviews__amount"]/text()').get()
+                item['free_fees'] = item_selection.xpath('.//div[contains(@class, "ui-search-item__group--price")]/span/text()').get()
+                items.append(item)
+                print(idx, item)
+        
+        """next_page_button_link = response.xpath('//div[@class="ui-search-pagination"]/ul/li[contains(@class, "__button--next")]/a/@href').get()
+        if next_page_button_link:
+            yield response.follow(next_page_button_link, self.parse_items, cb_kwargs={'items': items})"""
+        
+        print('*'*10)
+        print('\n\n')
+
     def parse(self, response):
+        # pages = int(getattr(self, 'pages', '5'))
         samsung_filter_button_link = response.xpath('//h3[contains(@class, "__item__header") and contains(text(),"Samsung")]/parent::a/@href').get()
         if samsung_filter_button_link:
-            yield response.follow(samsung_filter_button_link, callback=self.parse_items)
-    
-    def parse_items(self, response):
-        print('*'*10)
-        print('\n\n')
-        titles = response.xpath('//li[contains(@class, "item")]/div[contains(@class, "wrapper")]/div/div[contains(@class, "content-wrapper")]/div[contains(@class, "title")]/a/h2/text()').getall()
-        for title in titles:
-            print(title)
-        print('*'*10)
-        print('\n\n')
+            yield response.follow(samsung_filter_button_link, callback=self.parse_items, cb_kwargs={'items':[]})
