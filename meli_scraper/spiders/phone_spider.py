772e6b549c8d5e4f4e37974b469eaa89dbf8b647
Fabian Ardila <fabianra97@gmail.com>
2022-01-11 21:03:47 -0500

    :sparkles :

--- meli_scraper/spiders/phone_spider.py
+++ meli_scraper/spiders/phone_spider.py
@@ -33,8 +33,6 @@ class PhoneSpider(scrapy.Spider):
                 item['reviews'] = item_selection.xpath('.//span[@class="ui-search-reviews__amount"]/text()').get()
                 item['free_fees'] = item_selection.xpath('.//div[contains(@class, "ui-search-item__group--price")]/span/text()').get() # TODO: process to just get the number
                 items.append(item)
-        
-        # TODO: Add var quantity to any output
 
         next_page_button_link = response.xpath('//div[@class="ui-search-pagination"]/ul/li[contains(@class, "__button--next")]/a/@href').get()
         if next_page_button_link and pages > 0:
@@ -46,7 +44,7 @@ class PhoneSpider(scrapy.Spider):
             }
 
     def parse(self, response):
-        pages = int(getattr(self, 'pages', '1')) # TODO: set default pages to 5
+        pages = int(getattr(self, 'pages', '5'))
         samsung_filter_button_link = response.xpath('//h3[contains(@class, "__item__header") and contains(text(),"Samsung")]/parent::a/@href').get()
         if samsung_filter_button_link:
             yield response.follow(samsung_filter_button_link, callback=self.parse_items, cb_kwargs={'items':[], 'pages':pages, 'quantity': 0})
